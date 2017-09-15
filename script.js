db26161b1e399f29182656fb37fcd24fa3eac533
Augusto <gtoborges@gmail.com>
2017-09-14 21:45:33 -0300

    subindo arquivos

---
+++ script.js
@@ -0,0 +1,46 @@
+function alertIMC(){
+            var formulario = document.getElementById("formulario");
+
+            var peso = formulario.peso.value;
+            var altura = formulario.altura.value;
+            var sexo = formulario.sexo.value;
+
+            
+            var imc = peso/(altura*altura);
+            
+            if(sexo = "masculino"){
+                if(imc<=20.7){
+                    alert("Seu IMC: "+imc+"\nAbaixo do peso");
+                }
+                else if(imc>20.7 && imc<=26.4){
+                    alert("Seu IMC: "+imc+"\nPeso normal");
+                }
+                else if(imc>26.4 && imc<=27.8){
+                    alert("Seu IMC: "+imc+"\nLeve sobrepeso");
+                }
+                else if(imc>27.8 && imc<=31.1){
+                    alert("Seu IMC: "+imc+"\nAcima do peso ideal");
+                }
+                else if(imc>31.1){
+                    alert("Seu IMC: "+imc+"\nObeso");
+                }
+                
+            }
+            else{
+                if(imc<=19.1){
+                    alert("Seu IMC: "+imc+"\nAbaixo do peso");
+                }
+                else if(imc>19.1 && imc<=25.8){
+                    alert("Seu IMC: "+imc+"\nPeso normal");
+                }
+                else if(imc>25.8 && imc<=27.3){
+                    alert("Seu IMC: "+imc+"\nLeve sobrepeso");
+                }
+                else if(imc>27.3 && imc<=32.3){
+                    alert("Seu IMC: "+imc+"\nAcima do peso ideal");
+                }
+                else if(imc>32.3){
+                    alert("Seu IMC: "+imc+"\nObeso");
+                }
+            }
+}
